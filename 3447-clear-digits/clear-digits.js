5d73551b507172d14ee8603cf55120828a34847d
Yashwanth Mandya Nagesh <yashwanthmn20@gmail.com>
2025-02-11 18:16:45 -0500

    Time: 4 ms (25.89%) | Memory: 50.5 MB (97.88%) - LeetSync

--- 3447-clear-digits/clear-digits.js
+++ 3447-clear-digits/clear-digits.js
@@ -6,18 +6,20 @@ var clearDigits = function(s) {
     let flag = 0;
     let sArray = s.split('');
     let sLen = sArray.length;
+    let sLen1 = 0;
     while(!flag){
-        for(let i=0; i<sLen; i++){
-            
+        if(sArray.length==0){
+            break;
+        }
+        for(let i=0; i<sArray.length; i++){
             if(sArray[i]>=0 && sArray[i]<=9){
                 sArray.splice(i,1);
                 if(i!=0){
                     sArray.splice(i-1,1);
                 }
-             
                 break;
             }
-            if(i==sLen-1){
+            if(i==sArray.length-1){
                 flag=1;
             }   
              
