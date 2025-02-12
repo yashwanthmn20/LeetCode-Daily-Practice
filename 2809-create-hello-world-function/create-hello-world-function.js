eb769e6d36747098f264285c2c854c7de8e59d52
Yashwanth Mandya Nagesh <yashwanthmn20@gmail.com>
2025-02-11 22:53:49 -0500

    Time: 63 ms (5.93%) | Memory: 48.9 MB (28.04%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+
+var createHelloWorld = function() {
+    return () => "Hello World";
+    return function(...args) {
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
