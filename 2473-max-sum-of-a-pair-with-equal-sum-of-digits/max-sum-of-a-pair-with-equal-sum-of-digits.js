b3676fa46f1350157f179d8961bec83179bcc955
Yashwanth Mandya Nagesh <yashwanthmn20@gmail.com>
2025-02-11 21:05:03 -0500

    Time: 135 ms (22.35%) | Memory: 63.7 MB (54.12%) - LeetSync

---
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumSum = function(nums) {
+    nums.sort((a, b) => b - a);
+    console.log(nums);
+    
+    const hashFunc = new Map();
+    const numLen = nums.length;
+    let tempArray = [];
+    let tempSum = 0;
+    let answer = 0;
+    let flag = 0;
+
+    for(let i=0 ; i<numLen ; i++){
+        // let index = hashFunc.get(nums[i]);
+        let sum = 0;
+        let temp = nums[i];
+        while(temp!=0){
+            sum = sum + temp%10;
+            temp=Math.floor(temp/10);
+        }
+        console.log(sum);
+
+        if((hashFunc.has(sum))){
+            console.log(nums[i],(hashFunc.get(sum)));
+            answer = Math.max(answer,(nums[i])+(hashFunc.get(sum)));
+            flag = 1;
+        }
+        if(answer > (nums[0]+nums[i])){
+                return answer;
+            }
+
+        hashFunc.set(sum,nums[i]);
+    }
+    
+    if(flag==1){
+        return answer;
+    }else{
+        return -1;
+    }
+    
+};
