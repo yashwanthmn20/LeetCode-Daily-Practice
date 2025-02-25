1149c5d90e9461761a626e174cf4df3e2cd7be4d
Yashwanth Mandya Nagesh <yashwanthmn20@gmail.com>
2025-02-24 21:20:04 -0500

    Time: 24 ms (14.39%) | Memory: 64.3 MB (5.25%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    let nums3 = [...nums1,...nums2];
+
+    nums3.sort((a, b) => a - b);
+
+    let numsLen = nums3.length;
+    console.log(numsLen);
+
+    if(numsLen%2 == 0){
+        return((nums3[(numsLen/2)]+nums3[(numsLen/2)-1])/2);
+    }else{
+        return(nums3[Math.floor(numsLen/2)]);
+    }
+};
