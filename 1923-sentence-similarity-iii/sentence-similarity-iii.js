7c38f2ac9b753ffc20b13581bc6a94bdf6192c63
Yashwanth Mandya Nagesh <yashwanthmn20@gmail.com>
2025-03-03 23:27:33 -0500

    Time: 0 ms (100.00%) | Memory: 55 MB (5.26%) - LeetSync

---
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} sentence1
+ * @param {string} sentence2
+ * @return {boolean}
+ */
+var areSentencesSimilar = function(sentence1, sentence2) {
+    let s1Split = sentence1.split(" ");
+    let s2Split = sentence2.split(" ");
+
+    let s1Len = s1Split.length;
+    let s2Len = s2Split.length;
+    
+    let counter = 0 ;
+
+    let loopLen = (s1Len>s2Len)?s2Len:s1Len;
+
+
+
+    if(sentence1==sentence2){
+            return true;
+    }else if(s1Split[0]==s2Split[0] || s1Split[s1Len-1]==s2Split[s2Len-1]){
+        for(let i=0 ; i<loopLen ; i++){
+                if(s1Split[i]==s2Split[i]){
+                    counter++;
+                    matched = true;
+                }else{
+                    break;
+                }
+            }
+        for(let i=1; i<=loopLen; i++){
+            //console.log(s1Split[s1Len-i],s2Split[s2Len-i],loopLen,i)
+            if(s1Split[s1Len-i]==s2Split[s2Len-i]){
+                    counter++;
+                    matched = true;
+                }else{
+                    break;
+                }
+        }
+        matched = (counter>=loopLen)?true:false;
+        return matched;
+    }else{
+        return false;
+    }
+};
